3792807996ec10533e6492f0cd303dad583df2c4
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-08-09 20:34:11 +0300

    start 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+  bodyBg: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let intervalID;
+// = setInterval(onClickStart, 1000)
+function onClickStart() {
+  let colorBg = getRandomHexColor();
+  refs.bodyBg.style.backgroundColor = `${colorBg}`;
+}
+
+function onClickStop() {
+  clearTimeout(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener('click', onClickStart);
+refs.stopBtn.addEventListener('click', onClickStop);
