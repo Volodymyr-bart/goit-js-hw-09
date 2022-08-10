e3229d9e570632b915c8a0068622062853867da6
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-08-10 21:55:42 +0300

    add css

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,4 +3,21 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const refs = {
   dateInput: document.getElementById(`datetime-picker`),
+  startBtn: document.querySelector('[data-start]'),
+  dDays: document.querySelector('[data-days]'),
+  dHours: document.querySelector('[data-hours]'),
+  dMinutes: document.querySelector('[data-minutes]'),
+  dSeconds: document.querySelector('[data-seconds]'),
 };
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const libFp = flatpickr('#datetime-picker', options);
