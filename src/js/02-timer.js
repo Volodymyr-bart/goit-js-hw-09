d1b9ed5a4647f1823782c20ba8900f26f5a06ade
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-08-11 18:57:59 +0300

    edit task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,14 +9,82 @@ const refs = {
   dMinutes: document.querySelector('[data-minutes]'),
   dSeconds: document.querySelector('[data-seconds]'),
 };
+let currentTime = null;
+let userTime = null;
+refs.startBtn.disabled = true;
+let deltaTime = null;
 
+class Timer {
+  constructor({ onTick }) {
+    this.intervalID = null;
+    this.isActive = false;
+    this.onTick = onTick;
+  }
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.intervalID = setInterval(() => {
+      currentTime = Date.now();
+      deltaTime = convertMs(userTime - currentTime);
+      this.onTick(deltaTime);
+    }, 1000);
+  }
+  // stop() {
+  //   clearInterval(this.intervalID);
+  // }
+}
+
+const timer = new Timer({ onTick: updateClockFace });
+
+refs.startBtn.addEventListener('click', timer.start());
+
+function updateClockFace(deltaTime) {
+  const { days, hours, minutes, seconds } = deltaTime;
+  refs.dDays.textContent = `${days}`;
+  refs.dHours.textContent = `${hours}`;
+  refs.dMinutes.textContent = `${minutes}`;
+  refs.dSeconds.textContent = `${seconds}`;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, `0`);
+}
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    currentTime = Date.now();
+    if (currentTime > selectedDates[0]) {
+      alert('Please choose a date in the future');
+      refs.startBtn.disabled = true;
+    }
+    userTime = selectedDates[0];
+    refs.startBtn.disabled = false;
+    // deltaTime = convertMs(selectedDates[0] - currentTime);
   },
 };
 
